55954c26601b9b67b44571c51b8d6e66424cfc11
Maya-Coding-Kali <mayahermeskali@gmail.com>
2023-02-07 08:31:22 -0800

    For Class

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const first_name_text = document.getElementById("first-name");
+const last_name_text = document.getElementById("last-name");
+const checkbox = document.querySelectorAll("[type='checkbox']");
+let first_name = ""
+first_name_text.addEventListener("change", (event) => {
+    console.log(event.target.value);
+    first_name = event.target.value
+});
+// make a change event for last name and make the first letter always be uppercase
+//make a keyup for email and check for an @ symbol
+
+checkbox.forEach((element) => {
+    element.addEventListener("change", (event) => {
+      console.log(element.checked)
+      console.log(event.target.value);
+  });
+});
+//make a radio button event like the checkbox
+
